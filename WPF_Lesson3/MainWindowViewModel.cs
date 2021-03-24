a4e1a60ecdf8a0c5f3edc7262fd292e26a2628a7
User <User@DESKTOP-0BJFLEG>
2021-03-24 21:34:34 +0200

    Add project files.

---
+++ WPF_Lesson3/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Lesson3
+{
+    public class MainWindowViewModel
+    {
+        public Person MyPerson { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MyPerson = new Person { Name = "Danny", Age = 44 };
+        }
+
+        public void ChangePersonNameAndAge(string newName, int newAge)
+        {
+            MyPerson.Name = newName;
+            MyPerson.Age = newAge;
+            // also age = age + 1
+        }
+    }
+
+}
---
+++ WPF_Lesson3/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Lesson3
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string m_name;
+
+        private int m_age;
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+            set
+            {
+                m_name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return m_age;
+            }
+            set
+            {
+                m_age = value;
+                NotifyPropertyChanged("Age");
+            }
+        }
+
+        // Add Age property + PropertyChanged
+
+        // add function that is doing :
+        // NotifyPropertyChanged(string property_name)
+        // if (PropertyChanged != null)
+        // {
+        //PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+        //}
+        // call this function from Age + Name set property
+
+        public void NotifyPropertyChanged(string property_name) {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property_name));
+            }
+        }
+        public override string ToString()
+        {
+            return $"Person {Name} {Age}";
+        }
+    }
+
+}
